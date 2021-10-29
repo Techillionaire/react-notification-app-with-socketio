280edbf2ed2908929696400d0390e3e8b457a72a
techillionaire <techillionaire@gmail.com>
2021-10-29 09:53:50 +0100

    finito

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,46 @@
+import { useEffect, useState } from "react";
 import "./app.css";
-import { useState } from "react";
+import Card from "./components/card/Card";
+import Navbar from "./components/navbar/Navbar";
+import { posts } from "./data";
+import { io } from "socket.io-client";
 
 const App = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState("");
+  const [socket, setSocket] = useState(null);
 
-  const[username, setUserName] = useState("");
-  const [user, setUser] = useState(""); 
+  useEffect(() => {
+    setSocket(io("http://localhost:5000"));
+  }, []);
+
+  useEffect(() => {
+    socket?.emit("newUser", user);
+  }, [socket, user]);
 
   return (
-    <div>
-      <div className="login">
-        <input 
-          type="text"  
-          placeholder="username" 
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        <button onClick={() => setUser(username)}>Login</button>
-      </div>
+    <div className="container">
+      {user ? (
+        <>
+          <Navbar socket={socket} />
+          {posts.map((post) => (
+            <Card key={post.id} post={post} socket={socket} user={user}/>
+          ))}
+          <span className="username">{user}</span>
+        </>
+      ) : (
+        <div className="login">
+          <h2>Kings App</h2>
+          <input
+            type="text"
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button onClick={() => setUser(username)}>Login</button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
